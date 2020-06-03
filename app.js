0e83916e166b121f62f44948ec25a6da63ac06b2
Leenamkwon <namkwon12@gmail.com>
2020-06-03 13:27:17 +0900

    오류나서 다시 작성

--- app.js
+++ app.js
@@ -12,175 +12,23 @@ let editElement;
 let editFlag = false;
 let editID = '';
 
-// ****** EVENT LISTENERS **********
-// submit form
 form.addEventListener('submit', addItem);
-// clear items
-clearBtn.addEventListener('click', clearItems);
-// load items
-window.addEventListener('DOMContentLoaded', setupItems);
-// ****** FUNCTIONS **********
+
 function addItem(e) {
   e.preventDefault();
   const value = todoTemp.value;
   const id = new Date().getTime().toString();
-  if (value && !editFlag) {
-    createListItem(id, value);
 
-    // display alert
-    displayAlert('할 일이 추가되었습니다!', 'success');
-    // show container
-    container.classList.add('show-container');
-    // add to local storage
-    addToLocalStorage(id, value);
-    // set back to default
-    setBackToDefault();
-  } else if (value && editFlag) {
-    editElement.innerHTML = value;
-    displayAlert('수정이 완료 되었습니다.');
-    editFlag = false;
-    editLocalStorage(editID, value);
-    setBackToDefault();
+  if (value.trim() && !editFlag) {
+    const div = document.createElement('div');
+  } else if (value.trim() && editFlag) {
   } else {
-    displayAlert('내용을 입력해 주세요', 'danger');
+    showFeedBack('값을 입력해주세요', 'alert-danger');
   }
 }
 
-// DISPLY ALERT
-function displayAlert(text, action) {
-  alert.textContent = `${text}`;
-  alert.classList.add(`alert-${action}`);
+function showFeedBack() {}
 
-  // remove alert
-  setTimeout(() => {
-    alert.textContent = '';
-    alert.classList.remove(`alert-${action}`);
-  }, 1000);
-}
-
-// set back to default
-function setBackToDefault() {
-  todoTemp.value = '';
-  submitBtn.textContent = '추가';
-  editID = '';
-}
-
-// CLEAR ITEMS
-function clearItems() {
-  const items = document.querySelectorAll('.grocery-item');
-
-  if (items.length > 0) {
-    items.forEach((item) => {
-      list.removeChild(item);
-    });
-  }
-  container.classList.remove('show-container');
-  displayAlert('할 일을 모두 지웠습니다.', 'danger');
-  setBackToDefault();
-  localStorage.removeItem('list');
-}
-
-// DELETE FUNTION
-function deleteItem(e) {
-  const article = e.currentTarget.parentElement.parentElement;
-  const id = article.dataset.id;
-  list.removeChild(article);
-
-  // 리스트에 아무 것도 없다면 리스트칸 사라지게 하기
-  if (list.children.length === 0) {
-    container.classList.remove('show-container');
-  }
-  displayAlert('선택한 할 일을 지웠습니다.', 'danger');
-  // remove from localstorage
-  removeFromLocalStorage(id);
-}
-
-// EDIT FUNTION
-function editBtns(e) {
-  const article = e.currentTarget.parentElement.parentElement;
-  // set edit item
-  editElement = e.currentTarget.parentElement.previousElementSibling;
-
-  // set form value
-  todoTemp.value = editElement.innerHTML;
-  todoTemp.focus();
-  editFlag = true;
-  editID = article.dataset.id;
-  submitBtn.textContent = 'edit';
-}
-
-// ****** LOCAL STORAGE **********
-function addToLocalStorage(id, value) {
-  const todos = { id, value };
-  let items = localStorage.getItem('list')
-    ? JSON.parse(localStorage.getItem('list'))
-    : [];
-  items.push(todos);
-
-  localStorage.setItem('list', JSON.stringify(items));
-}
-function removeFromLocalStorage(id) {
-  let items = localStorage.getItem('list')
-    ? JSON.parse(localStorage.getItem('list'))
-    : [];
-
-  items = items.filter((item) => {
-    if (item.id !== id) {
-      return item;
-    }
-  });
-
-  localStorage.setItem('list', JSON.stringify(items));
-}
-
-function editLocalStorage(id, value) {
-  let items = localStorage.getItem('list')
-    ? JSON.parse(localStorage.getItem('list'))
-    : [];
-  items = items.map((item) => {
-    if (item.id === id) {
-      item.value = value;
-    }
-    return item;
-  });
-
-  localStorage.setItem('list', JSON.stringify(items));
-}
-// ****** SETUP ITEMS **********
-function setupItems() {
-  let items = localStorage.getItem('list')
-    ? JSON.parse(localStorage.getItem('list'))
-    : [];
-  if (items.length > 0) {
-    items.forEach((item) => {
-      createListItem(item.id, item.value);
-    });
-    container.classList.add('show-container');
-  }
-}
-
-function createListItem(id, value) {
-  const element = document.createElement('article');
-  // add class
-  element.classList.add('grocery-item');
-  // add id
-  const attr = document.createAttribute('data-id');
-  attr.value = id;
-  element.setAttribute('data-id', id);
-  element.innerHTML = `            
-  <p class="title">${value}</p>
-  <div class="btn-container">
-    <button type="button" class="edit-btn">
-      <i class="fas fa-edit"></i>
-    </button>
-    <button type="button" class="delete-btn">
-      <i class="fas fa-trash"></i>
-    </button>
-  </div>`;
-  const deleteBtn = element.querySelector('.delete-btn');
-  const editItem = element.querySelector('.edit-btn');
-  deleteBtn.addEventListener('click', deleteItem);
-  editItem.addEventListener('click', editBtns);
-  // apend child
-  list.appendChild(element);
-}
+// edit option이 필요한 이유?
+// edit값을 맞추려고?
+// editID는 상태 비교용 ?
